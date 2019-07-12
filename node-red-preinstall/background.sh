0192b2356ed89fb2617a40d649f29d1fb6adcd75
tseigo <tnk.seigo+bitbucket@gmail.com>
2019-07-12 15:40:25 +0900

    Updated

--- node-red-preinstall/background.sh
+++ node-red-preinstall/background.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "test"
-
 node -v
 npm -v
 sudo npm install -g --unsafe-perm node-red
--- node-red-preinstall/foreground.sh
+++ node-red-preinstall/foreground.sh
@@ -1,4 +1,10 @@
 echo "This is automatically run when the scenario"
 
+node -v
+npm -v
+sudo npm install -g --unsafe-perm node-red
+
 pwd
 ls -lha
+
+node-red
