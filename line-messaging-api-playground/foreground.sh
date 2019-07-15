68963e653de2db97e6b07e27a317b3cd20a9090d
tseigo <tnk.seigo+bitbucket@gmail.com>
2019-07-15 23:33:02 +0900

    Updated

--- line-messaging-api-playground/foreground.sh
+++ line-messaging-api-playground/foreground.sh
@@ -1,56 +1,8 @@
 echo "This is automatically run when the scenario"
 
-cat <<EOF > /home/scrapbook/tutorial/server.js
-'use strict';
+git clone https://github.com/1ft-seabass/katacoda-line-messaging-api-playground.git
 
-const express = require('express');
-const line = require('@line/bot-sdk');
-const PORT = process.env.PORT || 3000;
-
-const config = {
-    channelSecret: 'channelSecret',
-    channelAccessToken: 'channelAccessToken'
-};
-
-// 日本語
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)'));
-app.post('/webhook', line.middleware(config), (req, res) => {
-    console.log(req.body.events);
-
-    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
-        res.send('Hello LINE BOT!(POST)');
-        console.log('Connecting Check!');
-        return; 
-    }
-
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result));
-});
-
-const client = new line.Client(config);
-
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
-
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text
-  });
-}
-
-app.listen(PORT);
-console.log("Server running at port " + PORT);
-EOF
-
-
-npm init -y
-
-npm i express @line/bot-sdk
+npm i
 
 pwd
 
