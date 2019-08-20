b777044dcd06532d3a9e11016a1df51c9dff5e64
tseigo <tnk.seigo+bitbucket@gmail.com>
2019-08-20 11:37:41 +0900

    Updated

--- node-red-playground/foreground.sh
+++ node-red-playground/foreground.sh
@@ -8,3 +8,28 @@ pwd
 ls -lha
 
 node-red
+
+# ADD PASSWORD #########################
+
+npm install bcryptjs -g
+
+# interactive
+echo -n "Please wnter your Node-RED password. Your Node-RED ID is admin."
+read YOUR_NODERED_PASSWORD
+
+echo "# nodered password crypt make"
+UI_NODERED_PASSWORD_CRYPT=`node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 8));" ${YOUR_NODERED_PASSWORD}`
+
+echo "# nodered password template push"
+sed -i -e "s/\/\/adminAuth:/adminAuth:{\n\
+        type: \"credentials\",\n\
+        users: [{\n\
+            username: \"admin\",\n\
+            password: \"SACLOUD_NODERED_PASSWORD\",\n\
+            permissions: \"*\"\n\
+        }]\n\
+    },\n\
+    \/\/adminAuth:/" /root/.node-red/settings.js
+
+echo "# nodered password crypt change"
+sed -i -e "s*SACLOUD_NODERED_PASSWORD*$UI_NODERED_PASSWORD_CRYPT*" /root/.node-red/settings.js
