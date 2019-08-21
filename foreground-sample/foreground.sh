66be9a382f0f020c2f47ed319f60b24c420e6069
tseigo <tnk.seigo+bitbucket@gmail.com>
2019-08-21 14:53:51 +0900

    upload

--- foreground-sample/foreground.sh
+++ foreground-sample/foreground.sh
@@ -1,15 +1,10 @@
 echo "This is automatically run when the scenario"
-echo "v 0.0.3"
+echo "v 0.0.4"
 
 pwd
 
 ls -lha .
 
-echo "Waiting to complete"
-
-while [ ! -f /opt/.backgroundfinished ]
-  do
-    sleep 2
-done
+echo "Waiting to complete"; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2;
 
 echo "Done"
