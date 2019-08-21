08d0e5e37ca73e1382c83143fbc86e2918db6afd
tseigo <tnk.seigo+bitbucket@gmail.com>
2019-08-21 14:28:24 +0900

    test

--- foreground-sample/background.sh
+++ foreground-sample/background.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-
+while true
+do
+  echo "work!"
+  sleep 1
+done
--- foreground-sample/foreground.sh
+++ foreground-sample/foreground.sh
@@ -1,5 +1,10 @@
 echo "This is automatically run when the scenario"
+echo "v 0.0.1"
 
 pwd
 
 ls -lha .
+
+echo $$
+
+ps -f | egrep "$$|PID"
