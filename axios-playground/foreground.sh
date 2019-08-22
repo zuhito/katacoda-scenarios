ca5ac0fb728c7c5ef15c7ed07cde76eb9e4f2e35
tseigo <tnk.seigo+bitbucket@gmail.com>
2019-08-23 00:36:59 +0900

    upload

--- axios-playground/foreground.sh
+++ axios-playground/foreground.sh
@@ -1,8 +1,12 @@
 echo "This is automatically run when the scenario"
 
+cd ..
+
+mv example/* tutorial/*
+
 git clone https://github.com/1ft-seabass/katacoda-axios-playground.git example
 
-cd example
+cd tutorial
 
 npm i
 
