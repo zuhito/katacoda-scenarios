b834e59805a1ead8fed4ea67c710ebef1e94cf77
zuhito <zuhito@users.noreply.github.com>
2019-09-14 22:51:26 +0900

    Update foreground.sh

--- node-red-playground/foreground.sh
+++ node-red-playground/foreground.sh
@@ -1,37 +1,4 @@
-echo "This is automatically run when the scenario"
-echo "node-red-playground version 0.0.18"
-
 node -v
 npm -v
-sudo npm install -g --unsafe-perm node-red
-
-npm install bcryptjs
-YOUR_NODERED_SETTING_DIR=/root/.node-red/settings.js
-YOUR_NODERED_PASSWORD=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 10 | head -1)
-UI_NODERED_PASSWORD_CRYPT=`node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 8));" ${YOUR_NODERED_PASSWORD}`
-
-pwd
-
-# node-red
-
-#############
-
-timeout 5 node-red > /dev/null
-
-# sed -i -e "s/\/\/adminAuth:/adminAuth:{ type: \"credentials\", users: [{ username: \"admin\", password: \"CLOUD_NODERED_PASSWORD\", permissions: \"*\" }] },/" $YOUR_NODERED_SETTING_DIR
-
-sed -i -e "s/\/\/adminAuth:/adminAuth:{\x0A\
-        type: \"credentials\",\x0A\
-        users: [{\x0A\
-            username: \"admin\",\x0A\
-            password: \"CLOUD_NODERED_PASSWORD\",\x0A\
-            permissions: \"*\"\x0A\
-        }]\x0A\
-    },\x0A\
-    \/\/adminAuth:/" $YOUR_NODERED_SETTING_DIR
-
-sed -i -e "s*CLOUD_NODERED_PASSWORD*$UI_NODERED_PASSWORD_CRYPT*" /root/.node-red/settings.js
-
-echo "This Node-RED already is made. ID is admin and PASSWORD is ${YOUR_NODERED_PASSWORD}."
-
-echo "Let's input node-red and click ENTER"
+sudo npm install -g --unsafe-perm node-red@next
+node-red
