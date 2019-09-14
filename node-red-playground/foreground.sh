bb5f0e14a4ab21a86940621759d8151a2a65a65d
zuhito <zuhito@users.noreply.github.com>
2019-09-15 01:38:27 +0900

    Update foreground.sh

--- node-red-playground/foreground.sh
+++ node-red-playground/foreground.sh
@@ -6,6 +6,7 @@ git clone https://github.com/node-red/node-red.git
 cd node-red
 git checkout dev
 npm install --production
+npm install --production grunt
 npm run build
 mkdir ~/.node-red
 sed -r "s/enabled: false/enabled: true/" packages/node_modules/node-red/settings.js > ~/.node-red/settings.js
