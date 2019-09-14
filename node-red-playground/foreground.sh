4858cedccacf84c9e27b85fe8d9cf82810aff133
zuhito <zuhito@users.noreply.github.com>
2019-09-15 01:34:08 +0900

    Update foreground.sh

--- node-red-playground/foreground.sh
+++ node-red-playground/foreground.sh
@@ -5,16 +5,16 @@ npm -v
 git clone https://github.com/node-red/node-red.git
 cd node-red
 git checkout dev
-npm install
+npm install --production
 npm run build
 mkdir ~/.node-red
 sed -r "s/enabled: false/enabled: true/" packages/node_modules/node-red/settings.js > ~/.node-red/settings.js
 cd ~/.node-red
-npm install git+https://github.com/node-red/node-red-dashboard.git
+npm install --production git+https://github.com/node-red/node-red-dashboard.git
 git clone https://github.com/node-red/node-red-ui-nodes.git
-npm install node-red-ui-nodes/node-red-node-ui-table
-npm install node-red-ui-nodes/node-red-node-ui-list
-npm install node-red-ui-nodes/node-red-node-ui-vega
-npm install git+https://github.com/node-red/node-red-node-swagger.git
+npm install --production node-red-ui-nodes/node-red-node-ui-table
+npm install --production node-red-ui-nodes/node-red-node-ui-list
+npm install --production node-red-ui-nodes/node-red-node-ui-vega
+npm install --production git+https://github.com/node-red/node-red-node-swagger.git
 cd ~/node-red
 npm start
