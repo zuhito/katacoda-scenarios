b7876bfa388e694144be8b9650cf1da144aa2697
zuhito <zuhito@users.noreply.github.com>
2019-09-14 22:54:25 +0900

    Update foreground.sh

--- node-red-playground/foreground.sh
+++ node-red-playground/foreground.sh
@@ -1,4 +1,9 @@
 node -v
 npm -v
-sudo npm install -g --unsafe-perm node-red@next
-node-red
+# sudo npm install -g --unsafe-perm node-red@next
+# node-red
+git clone https://github.com/node-red/node-red.git
+cd node-red
+npm install
+npm run build
+npm start
