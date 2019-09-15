88119bbe42150fd191afa69734c72275243a9475
zuhito <zuhito@users.noreply.github.com>
2019-09-15 12:36:05 +0900

    Update foreground.sh

--- node-red-playground/foreground.sh
+++ node-red-playground/foreground.sh
@@ -8,15 +8,15 @@ git checkout dev
 npm install
 npm run build
 mkdir ~/.node-red
-sed -r "s/enabled: false/enabled: true/" packages/node_modules/node-red/settings.js > ~/.node-red/settings.js
 cd ~/.node-red
+sed -r "s/enabled: false/enabled: true/" ~/node-red/packages/node_modules/node-red/settings.js > settings.js
 npm install git+https://github.com/node-red/node-red-dashboard.git
 git clone https://github.com/node-red/node-red-ui-nodes.git
 ls -d node-red-ui-nodes/*/ | grep node-red-node-ui- | xargs -I{} npm install {}
-git clone https://github.com/node-red/node-red-nodes.git
-ls -d node-red-nodes/*/*/ | xargs -I{} npm install {}
-git clone https://github.com/node-red/node-red-web-nodes.git
-ls -d node-red-web-nodes/*/ | xargs -I{} npm install {}
+# git clone https://github.com/node-red/node-red-nodes.git
+# ls -d node-red-nodes/*/*/ | xargs -I{} npm install {}
+# git clone https://github.com/node-red/node-red-web-nodes.git
+# ls -d node-red-web-nodes/*/ | xargs -I{} npm install {}
 npm install git+https://github.com/node-red/node-red-node-swagger.git
 cd ~/node-red
 npm start
