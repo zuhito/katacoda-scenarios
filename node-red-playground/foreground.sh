10090ee267d3829492a1b597ddc21179b14525d9
zuhito <zuhito@users.noreply.github.com>
2019-09-15 12:12:51 +0900

    Update foreground.sh

--- node-red-playground/foreground.sh
+++ node-red-playground/foreground.sh
@@ -12,9 +12,11 @@ sed -r "s/enabled: false/enabled: true/" packages/node_modules/node-red/settings
 cd ~/.node-red
 npm install git+https://github.com/node-red/node-red-dashboard.git
 git clone https://github.com/node-red/node-red-ui-nodes.git
-npm install node-red-ui-nodes/node-red-node-ui-table
-npm install node-red-ui-nodes/node-red-node-ui-list
-npm install node-red-ui-nodes/node-red-node-ui-vega
+ls -d node-red-ui-nodes/*/ | grep node-red-node-ui- | xargs -I{} npm install {}
+git clone https://github.com/node-red/node-red-nodes.git
+ls -d node-red-nodes/*/*/ | xargs -I{} npm install {}
+git clone https://github.com/node-red/node-red-web-nodes.git
+ls -d node-red-web-nodes/*/ | xargs -I{} npm install {}
 npm install git+https://github.com/node-red/node-red-node-swagger.git
 cd ~/node-red
 npm start
