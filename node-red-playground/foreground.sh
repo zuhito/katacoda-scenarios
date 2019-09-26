abab9fc499d6966e94b5a7a248f97e33aaeb5309
zuhito <zuhito@users.noreply.github.com>
2019-09-27 00:35:04 +0900

    Update foreground.sh

--- node-red-playground/foreground.sh
+++ node-red-playground/foreground.sh
@@ -19,5 +19,27 @@ npm install node-red-ui-nodes/node-red-node-ui-table
 # git clone https://github.com/node-red/node-red-web-nodes.git
 # ls -d node-red-web-nodes/*/ | xargs -I{} npm install {}
 npm install git+https://github.com/node-red/node-red-node-swagger.git
+npm install node-red-contrib-flightradar24
+npm install node-red-contrib-model-asset-exchange
+npm install node-red-contrib-julius
+npm install node-red-contrib-cognitive-services
+npm install node-red-contrib-iss-location
+npm install node-red-contrib-sakura-io-datastore-api
+npm install node-red-contrib-sakura-io-datastore-api
+npm install node-red-contrib-embedded-file
+npm install node-red-contrib-java-function
+npm install node-red-contrib-cloud-vision-api
+npm install node-red-contrib-openjtalk
+npm install node-red-contrib-azure-iot-edge
+npm install node-red-contrib-iss-location
+npm install node-red-contrib-whois
+npm install node-red-contrib-sakura-io-datastore-api
+npm install node-red-contrib-google-translate
+npm install node-red-contrib-sendgrid
+npm install node-red-contrib-tensorflow
+npm install node-red-contrib-openjtalk
+npm install node-red-contrib-opencv
+npm install node-red-contrib-whois
+
 cd ~/node-red
 npm start
