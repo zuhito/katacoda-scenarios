c06283bdf02d04398521b4e151e5a29bc71cbf9c
zuhito <zuhito@users.noreply.github.com>
2019-10-07 12:42:19 +0900

    Update foreground.sh

--- raspberrypi/foreground.sh
+++ raspberrypi/foreground.sh
@@ -5,4 +5,3 @@ wget https://downloads.raspberrypi.org/raspbian_full_latest
 unzip raspbian_full_latest
 mv *.img raspbian_full_latest.img
 qemu-system-arm -kernel kernel-qemu-4.19.50-buster -dtb versatile-pb.dtb -m 256 -M versatilepb -cpu arm1176 -serial stdio -append "rw console=ttyAMA0 root=/dev/sda2 rootfstype=ext4 loglevel=8 rootwait fsck.repair=yes memtest=1" -drive file=fullraspbian_full_latest.img,format=raw -no-reboot -vga std -nographic -vnc :0
-
