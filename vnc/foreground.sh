e09f8cd0077bdc64722528615226b62fd159ca20
zuhito <zuhito@users.noreply.github.com>
2020-08-10 14:51:55 +0000

    Update foreground.sh

--- vnc/foreground.sh
+++ vnc/foreground.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:80 -e RESOLUTION=800x600 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
+docker run -p 8080:80 -e RESOLUTION=800x590 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
