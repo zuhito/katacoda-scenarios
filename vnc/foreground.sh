2c4c763d1185a8d7a728727d5316891c01022f63
Your Name <you@example.com>
2020-08-10 14:05:25 +0000

    vnc

--- vnc/foreground.sh
+++ vnc/foreground.sh
@@ -1,5 +1,2 @@
-apt-get install -y vnc4server
-printf "password\npassword\n\n" | vnc4passwd
-vncserver -geometry 1366x768
-sudo snap install novnc
+apt-get install -y vnc4server; printf "password\npassword\n\n" | vnc4passwd; vncserver -geometry 1366x768; sudo snap install novnc
 novnc --listen 8080 --vnc localhost:5901
